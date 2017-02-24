9b949a5e8900fb522723fd50ddad95863263c5ad
IdanWo <wolf.idan@gmail.com>
2017-02-24 11:50:52 +0200

    adding common project

---
+++ client/MamanNetClient/Common/LogUtilities/LogEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.LogUtilities
+{
+    public enum LogSeverity
+    {
+        Info,
+        Error,
+        CriticalError
+    }
+
+    [Serializable]
+    public class LogEntry
+    {
+        public string Entry { get; set; }
+        public LogSeverity LogSeverity { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ client/MamanNetClient/Common/LogUtilities/LogStoreProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.LogUtilities
+{
+    public static class LogStoreProvider
+    {
+        private static IFormatter BinaryFormatter { get; set; }
+        private static string FileName = "MamaNetLogStore.bin";
+
+        public static List<LogEntry> LoadLogs()
+        {
+            var savedLogEntries = new List<LogEntry>();
+            using (var fileStream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Read, FileShare.None))
+            {
+                BinaryFormatter = new BinaryFormatter();
+                while (fileStream.Position != fileStream.Length)
+                {
+                    savedLogEntries.Add((LogEntry)BinaryFormatter.Deserialize(fileStream));
+                }
+
+            }
+            return savedLogEntries;
+        }
+
+        public static void AppendLog(string entry, LogSeverity logSeverity)
+        {
+            var logEntry = new LogEntry() { Entry =  entry, LogSeverity = logSeverity, Timestamp = DateTime.Now};
+            
+            using (var fileStream = new FileStream(FileName, FileMode.Append, FileAccess.Write, FileShare.None))
+            {
+                BinaryFormatter = new BinaryFormatter();
+                BinaryFormatter.Serialize(fileStream, logEntry);
+            }
+        }
+    }
+}
